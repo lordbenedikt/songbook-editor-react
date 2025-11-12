bf571fd80c8f23f5b9be4131131fede8ec580a18
Benedikt Jensen <Benedikt.Jensen@pvatepla.com>
2025-11-12 18:12:32 +0100

    use envvar to resolve chrome path

--- src/pdf-server/src/server.ts
+++ src/pdf-server/src/server.ts
@@ -18,7 +18,7 @@ app.post("/generate-pdf", async (req, res) => {
                 "--disable-setuid-sandbox", // required on Linux
                 "--disable-dev-shm-usage",  // optional but safer on containers
             ],
-            executablePath: puppeteer.executablePath(), // ✅ this resolves the correct Chrome path
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
         });
         const page = await browser.newPage();
 
